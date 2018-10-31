22cfc6ecb181082fbe21b5288da9c0796121f513
Denis Raygoroskiy <paradoxetion@gmail.com>
2018-10-31 19:16:14 +0300

    App code structure and basic styling

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-    sample: 'Hello',
+    location: 'unknown',
+    degree: null,
+    degreeModes: ['C', 'F'],
+    currentDegreeMode: 'C',
+    relatedImgUrl: 'https://images.unsplash.com/photo-1533612773272-7230cca4169e'
   },
   methods: {
     
