4de6db790c68bbb68d209d929da119fa8d928b1d
Denis Raygoroskiy <paradoxetion@gmail.com>
2018-10-31 22:13:34 +0300

    function to get coordinates

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
 new Vue({
   el: '#app',
   data: {
-    location: 'unknown',
+    location: {
+      name: 'unknown',
+      lat: 0,
+      lon: 0,
+    },
     degree: null,
-    degreeModes: ['C', 'F'],
-    currentDegreeMode: 'C',
+    degreeModes: ['C°', 'F°'],
+    currentDegreeMode: 'C°',
     relatedImgUrl: 'https://images.unsplash.com/photo-1533612773272-7230cca4169e'
   },
   methods: {
-    
+    getCurrentLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+          this.location.lat = position.coords.latitude;
+          this.location.lon = position.coords.longitude;
+        });
+      } else { 
+        alert('Update your browser =) ')
+      }
+    }
   }
 })
